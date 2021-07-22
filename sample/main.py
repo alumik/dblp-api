62d97ce19ef31d6c835d5a643b7040b7258bf200
Zhong Zhenyu <nczzy1997@gmail.com>
2021-07-23 01:32:03 +0800

    Use python-fire to set input file path

--- sample/main.py
+++ sample/main.py
@@ -1,9 +1,16 @@
+import sys
+sys.path.append('..')
+
 import dblp
+import fire
 import pandas as pd
 
-with open('query.txt', 'r') as f:
-    queries = f.read().splitlines()
+def main(file: str = 'query.txt'):
+    with open(file, 'r') as f:
+        queries = f.read().splitlines()
+    results = dblp.search(queries, verbose=1)
+    print(results)
+    pd.DataFrame(results).to_csv('output.csv', index=False)
 
-results = dblp.search(queries, verbose=1)
-print(results)
-pd.DataFrame(results).to_csv('output.csv', index=False)
+if __name__ == '__main__':
+    fire.Fire(main)
